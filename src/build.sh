bb37f96cea733239f64451124260e16d4ff8a1e9
lwz7512 <lwz7512@gmail.com>
2018-12-30 18:22:56 -0400

    regenerate style and js...

--- src/build.sh
+++ src/build.sh
@@ -8,25 +8,25 @@ mkdir -p _assets/website/
 mkdir -p _assets/ebook/
 
 # Compile JS
-# browserify src/js/core/index.js | uglifyjs -mc > _assets/website/gitbook.js
+browserify src/js/core/index.js | uglifyjs -mc > _assets/website/gitbook.js
 echo 'browserify theme...'
 browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/theme.js
 echo 'theme rebuild!'
 
 # NOTE, modify 'yan' to your gitbook project directory
 # NOTE, yan is in the same directory as theme-default
-cp _assets/website/theme.js ../"$1"/_book/gitbook/
-cp _assets/website/theme.js ../"$1"/build/gitbook/
+#cp _assets/website/theme.js ../"$1"/_book/gitbook/
+#cp _assets/website/theme.js ../"$1"/build/gitbook/
 
 # Compile Website CSS
 lessc -clean-css src/less/website.less _assets/website/style.css
 
 # NOTE, modify 'yan' to your gitbook project directory
 # NOTE, yan is in the same directory as theme-default
-cp _assets/website/style.css ../"$1"/_book/gitbook/
-cp _assets/website/style.css ../"$1"/build/gitbook/
+#cp _assets/website/style.css ../"$1"/_book/gitbook/
+#cp _assets/website/style.css ../"$1"/build/gitbook/
 
-echo "copied theme.js and style.css to $1 !"
+#echo "copied theme.js and style.css to $1 !"
 
 # Compile eBook CSS
 # lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
